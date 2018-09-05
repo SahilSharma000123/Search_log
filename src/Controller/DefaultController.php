62726c6df44f464fed5b8f0d3d52de4b40cf0dd2
Anil <anil@Anils-Air.Dlink>
2018-09-05 15:24:30 +0530

    Added Install Files.

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\search_log\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class DefaultController.
+ */
+class DefaultController extends ControllerBase {
+
+  /**
+   * Search_log.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function search_log() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Implement method: search_log')
+    ];
+  }
+  /**
+   * Search_history.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function search_history() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Implement method: search_history')
+    ];
+  }
+
+}
